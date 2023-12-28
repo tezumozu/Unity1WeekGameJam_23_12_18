f70ce04c1a367de702850756777abeca59b5a307
tezumozu <htworkem@gmail.com>
2023-12-29 01:39:06 +0900

    add Sprites

--- 23_12_18/Assets/Script/TypingScene/Enum/E_Voice.cs
+++ 23_12_18/Assets/Script/TypingScene/Enum/E_Voice.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public enum E_Voice {
     First_1,
-    Latter_1
+    First_2,
+    First_3,
+    First_4,
+    First_5,
+    First_6,
+    First_7,
+    First_8,
+    First_9,
+    First_10,
+    First_11,
+    First_12,
+    First_13,
+    First_14,
+    First_15,
+    
+    Latter_1,
+    Latter_2,
+    Latter_3,
+    Latter_4,
+    Latter_5,
+    Latter_6,
+    Latter_7,
+    Latter_8,
+    Latter_9,
+    Latter_10,
+    Latter_11,
+    Latter_12,
+    Latter_13,
+    Latter_14,
+    Latter_15
 }
--- 23_12_18/Assets/Script/TypingScene/QuestionSet/E_LatterQuestion.cs
+++ 23_12_18/Assets/Script/TypingScene/QuestionSet/E_LatterQuestion.cs
@@ -3,5 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum E_LatterQuestion {
-    Data_1
+    Data_1,
+    Data_2,
+    Data_3,
+    Data_4,
+    Data_5,
+    Data_6,
+    Data_7,
+    Data_8,
+    Data_9,
+    Data_10,
+    Data_11,
+    Data_12,
+    Data_13,
+    Data_14,
+    Data_15
 }
